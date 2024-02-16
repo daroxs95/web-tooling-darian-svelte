d2fa0f4ccca5abcb7f75fe861a886aaf97092df8
Darián López Utra <daroxs95@gmail.com>
2024-02-16 03:09:26 +0100

    Add `Home` component

---
+++ src/lib/Home/Home.svelte
@@ -0,0 +1,16 @@
+<div class="fullscreen">
+  <slot />
+</div>
+
+<style>
+  .fullscreen {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background: var(--bg);
+  }
+</style>
